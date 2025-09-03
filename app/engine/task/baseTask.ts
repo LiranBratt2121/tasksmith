9cefee676373d8c1723919f59bb56051d5bd88aa
LiranBratt2121 <brattlirannin@gmail.com>
2025-09-03 16:11:30 +0300

    Modify baseTask to include state

--- app/engine/task/baseTask.ts
+++ app/engine/task/baseTask.ts
@@ -1,20 +1,38 @@
 "use client";
 
-import { FC, JSX } from "react";
-import { TaskData } from "./task.types";
+import { JSX } from "react";
+import { BaseTaskData } from "./task.types";
 
 export abstract class BaseTask {
-    protected task: TaskData;
+  protected data: BaseTaskData;
+  protected state: Record<string, any> = {};
 
-    constructor(task: TaskData) {
-        this.task = task;
-    }
+  constructor(data: BaseTaskData) {
+    this.data = data;
+  }
 
-    id() {
-        return this.task.id;
-    }
+  id() { 
+    return this.data.id; 
+  }
+  
+  getLocation() { 
+    return this.data.location; 
+  }
+  
+  setLocation(x: number, y: number) { 
+    this.data.location = { x, y }; 
+  }
+  
+  getData() { 
+    return this.data; 
+  }
+  
+  getState() { 
+    return this.state; 
+  }
 
-    abstract render(): JSX.Element;
-
-    abstract validate(): boolean;
+  abstract render(props?: Record<string, unknown>): JSX.Element;
+  abstract validate(): { isValid: boolean; message: string };
+  abstract getConfig(): any;
+  abstract clone(): BaseTask;
 }
