8abd963b343a8d8b9ac7a263d225ace9cd63e7ea
LiranBratt2121 <brattlirannin@gmail.com>
2025-09-04 18:13:56 +0300

    Use heebo as main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,19 @@
+import { Heebo } from 'next/font/google';
+
+const heebo = Heebo({
+  weight: '500',
+  subsets: ['latin'],
+})
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
+    <html lang="en" className={heebo.className}>
       <body>
         {children}
       </body>
